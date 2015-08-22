84adc4bfcdfff3cbf19fd96eb400d44beba37cfe
Allan BURDAJEWICZ <allan.burdajewicz@gmail.com>
2015-08-22 12:52:48 +1000

    #2 Tests for basic strategy and evaluation (pieces score and symmetry).

---
+++ test/evaluation/evaluator-test.js
@@ -0,0 +1,46 @@
+'use strict';
+var assert = require('assert');
+var evaluator = require('../../src/evaluation/evaluator');
+var chessRules = require('chess-rules');
+
+describe('evaluator', function () {
+    it('must provide a 0 score on initial board', function () {
+
+        var position = chessRules.getInitialPosition();
+
+        assert(evaluator.evaluatePosition(position, 'basic') == 0);
+    });
+
+    it('must provide logical score by central symmetry', function () {
+
+        var position = chessRules.getInitialPosition();
+
+        position.turn = 'W';
+        var whiteScore = evaluator.evaluatePosition(position, 'basic');
+        position.turn = 'B';
+        var blackScore = evaluator.evaluatePosition(position, 'basic');
+
+        assert(whiteScore === blackScore);
+    });
+
+
+    it('must provide a better score when pawn move to center', function () {
+
+        //White move
+        var position = chessRules.getInitialPosition();
+        var move = chessRules.pgnToMove(position, 'e4');
+
+        console.log(chessRules.positionToString(position, true));
+        var initialScore = evaluator.evaluatePosition(position, 'basic');
+
+        position = chessRules.applyMove(position, move);
+
+        console.log(chessRules.positionToString(position, true));
+        var blackScore = evaluator.evaluatePosition(position, 'basic');
+        position.turn = 'W';
+        var whiteScore = evaluator.evaluatePosition(position, 'basic');
+
+        assert(initialScore < whiteScore);
+        assert(initialScore > blackScore);
+    });
+});
--- test/evaluation/strategies/strategy-test.js
+++ test/evaluation/strategies/strategy-test.js
@@ -1,22 +1,102 @@
 'use strict';
 var assert = require('assert');
-//var alphaBetaSearch = require('../../../src/');
-//var chessRules = require('chess-rules');
+var strategy = require('../../../src/evaluation/strategy');
 
-describe('random strategy', function () {
-    it('must provide a score for each piece', function () {
-        /**
-         * TODO: Test for the strategy.
-         */
-        assert(true, 'Score is valid');
+describe('strategy-basic', function () {
+
+    it('must provide a score for a pawn', function () {
+        var whiteValue = strategy.getPieceScore({type: 'P', side:'W'}, 'basic');
+        var blackValue = strategy.getPieceScore({type: 'P', side:'B'}, 'basic');
+        assert(whiteValue === 100, 'Wrong value');
+        assert(whiteValue === blackValue, 'Not symmetric');
     });
-});
 
-describe('basic strategy', function () {
-    it('must provide a score for each piece position', function () {
-        /**
-         * TODO: Test for the strategy.
-         */
-        assert(true, 'Score is valid');
+    it('must provide a score for a knight', function () {
+        var whiteValue = strategy.getPieceScore({type: 'N', side:'W'}, 'basic');
+        var blackValue = strategy.getPieceScore({type: 'N', side:'B'}, 'basic');
+        assert(whiteValue === 300, 'Wrong value');
+        assert(whiteValue === blackValue, 'Not symmetric');
+    });
+
+    it('must provide a score for a bishop', function () {
+        var whiteValue = strategy.getPieceScore({type: 'B', side:'W'}, 'basic');
+        var blackValue = strategy.getPieceScore({type: 'B', side:'B'}, 'basic');
+        assert(whiteValue === 325, 'Wrong value');
+        assert(whiteValue === blackValue, 'Not symmetric');
+    });
+
+    it('must provide a score for a rook', function () {
+        var whiteValue = strategy.getPieceScore({type: 'R', side:'W'}, 'basic');
+        var blackValue = strategy.getPieceScore({type: 'R', side:'B'}, 'basic');
+        assert(whiteValue === 500, 'Wrong value');
+        assert(whiteValue === blackValue, 'Not symmetric');
+    });
+
+    it('must provide a score for each queen', function () {
+        var whiteValue = strategy.getPieceScore({type: 'Q', side:'W'}, 'basic');
+        var blackValue = strategy.getPieceScore({type: 'Q', side:'B'}, 'basic');
+        assert(whiteValue === 900, 'Wrong value');
+        assert(whiteValue === blackValue, 'Not symmetric');
+    });
+
+    it('must provide a score for a king', function () {
+        var whiteValue = strategy.getPieceScore({type: 'K', side:'W'}, 'basic');
+        var blackValue = strategy.getPieceScore({type: 'K', side:'B'}, 'basic');
+        assert(whiteValue === 32767, 'Wrong value');
+        assert(whiteValue === blackValue, 'Not symmetric');
+    });
+
+    it('must provide a score for each pawn position', function () {
+        var index;
+        for (index = 0; index < 64; index++) {
+            var whiteValue = strategy.getPositionScore({type: 'P', side:'W'}, index, 'basic');
+            var blackValue = strategy.getPositionScore({type: 'P', side:'B'}, 63-index, 'basic');
+            assert(whiteValue === blackValue, 'Not symmetric');
+        }
+    });
+
+    it('must provide a score for each knight position', function () {
+        var index;
+        for (index = 0; index < 64; index++) {
+            var whiteValue = strategy.getPositionScore({type: 'N', side:'W'}, index, 'basic');
+            var blackValue = strategy.getPositionScore({type: 'N', side:'B'}, 63-index, 'basic');
+            assert(whiteValue === blackValue, 'Not symmetric');
+        }
+    });
+
+    it('must provide a score for each bishop position', function () {
+        var index;
+        for (index = 0; index < 64; index++) {
+            var whiteValue = strategy.getPositionScore({type: 'B', side:'W'}, index, 'basic');
+            var blackValue = strategy.getPositionScore({type: 'B', side:'B'}, 63-index, 'basic');
+            assert(whiteValue === blackValue, 'Not symmetric');
+        }
+    });
+
+    it('must provide a score for each rook position', function () {
+        var index;
+        for (index = 0; index < 64; index++) {
+            var whiteValue = strategy.getPositionScore({type: 'R', side:'W'}, index, 'basic');
+            var blackValue = strategy.getPositionScore({type: 'R', side:'B'}, 63-index, 'basic');
+            assert(whiteValue === blackValue, 'Not symmetric');
+        }
+    });
+
+    it('must provide a score for each queen position', function () {
+        var index;
+        for (index = 0; index < 64; index++) {
+            var whiteValue = strategy.getPositionScore({type: 'Q', side:'W'}, index, 'basic');
+            var blackValue = strategy.getPositionScore({type: 'Q', side:'B'}, 63-index, 'basic');
+            assert(whiteValue === blackValue, 'Not symmetric');
+        }
+    });
+
+    it('must provide a score for each king position', function () {
+        var index;
+        for (index = 0; index < 64; index++) {
+            var whiteValue = strategy.getPositionScore({type: 'K', side:'W'}, index, 'basic');
+            var blackValue = strategy.getPositionScore({type: 'K', side:'B'}, 63-index, 'basic');
+            assert(whiteValue === blackValue, 'Not symmetric');
+        }
     });
 });
